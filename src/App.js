b91a8b69b0211d02c246427ebe83dc7ca133e98a
Tsan <65704223+sutsanyuan@users.noreply.github.com>
2026-04-08 18:01:12 +0800

    semantic update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
-
+import Header from "./Header";
+import Main from "./Main";
+import Footer from "./Footer";
 function App() {
-    return <div className="App">Home Page One line added with the iMAC</div>;
+    return (
+        <>
+            <Header />
+            <Main />
+            <Footer />
+        </>
+    );
 }
 
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Nav from "./Nav";
+
+export default function Header() {
+    return (
+        <header>
+            HEADER
+            <Nav />
+        </header>
+    );
+}
