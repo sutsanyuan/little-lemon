178c5e839b4a90ec0e263cb05438f6782ec92f64
Tsan <65704223+sutsanyuan@users.noreply.github.com>
2026-04-13 17:57:40 +0800

    router-structure

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Nav from "./Nav";
+import logo from "./assets/logo.png";
+import "./styles/Header.scss";
 
 export default function Header() {
     return (
         <header>
-            HEADER
-            <Nav />
+            <div className="container">
+                <img src={logo} alt="Little Lemon Logo" />
+                <Nav />
+            </div>
         </header>
     );
 }
