a424582303518b0fd1fbc482f06913efdde1d8ec
蘇粲淵 <sutsanyuan@sucanyuans-MacBook-Air.local>
2026-05-10 21:20:36 +0800

    starting Testimonials.js

--- src/pages/HomePage/Home.js
+++ src/pages/HomePage/Home.js
@@ -2,13 +2,14 @@ import React from "react";
 import "./Home.scss";
 import Hero from "./Hero";
 import Specials from "./Specials";
+import Testimonials from "./Testimonials";
 
 export default function Home() {
     return (
         <main>
             <Hero></Hero>
             <Specials></Specials>
-            
+            <Testimonials></Testimonials>
         </main>
     );
 }
