81bf92a62506afb1c0026c91ac1a26c07e52e645
Tsan <65704223+sutsanyuan@users.noreply.github.com>
2026-05-12 18:29:41 +0800

    building up Testimonial, check 0512

--- src/pages/HomePage/Testimonials.js
+++ src/pages/HomePage/Testimonials.js
@@ -1,12 +1,21 @@
 import React from "react";
+import TestimonialCard from "../../components/common/TestimonialCard";
+import useFetch from "../../hooks/useFetch";
+import { fetchTestimonials } from "../../data/testimonialsData";
 
 export default function Testimonials() {
+    const { data: testimonials, loading } = useFetch(fetchTestimonials);
+    //here 0512
+
     return (
         <div>
             <section className="">
                 <div className="container">
                     <hr></hr>
                     <h3 className="section-title">Testimonials</h3>
+                    <div className="testimonial-wrapper">{}</div>
+
+                    <TestimonialCard></TestimonialCard>
                 </div>
             </section>
         </div>
