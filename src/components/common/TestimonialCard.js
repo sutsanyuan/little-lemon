2ddc81bb44e299f34c29fab2a017e45bf5470731
Tsan <65704223+sutsanyuan@users.noreply.github.com>
2026-05-14 17:06:40 +0800

    Carousel applied and refine 1. useRef for saving memories without rerender the site ,2. useEffect for removing listener when leaving the page,3. universal container for all types of cards

--- src/components/common/TestimonialCard.js
+++ src/components/common/TestimonialCard.js
@@ -1,24 +1,25 @@
 import React from "react";
+import styles from "./TestimonialCard.module.scss";
 import starImage from "../../assets/icons/star.svg";
 
-export default function TestimonialCard({ image, name, point,comment }) {
+export default function TestimonialCard({ image, name, point, comment }) {
     return (
-        <article className="special-card">
-            <div className="card-header">
+        <article className={styles["special-card"]}>
+            <div className={styles["card-header"]}>
                 <figure>
-                    <picture className="portrait-image">
+                    <picture className={styles["portrait-image"]}>
                         <img src={image} alt={name} />
                     </picture>
                     <h4>{name}</h4>
                 </figure>
-                <div className="stars">
+                <div className={styles["stars"]}>
                     <div>
                         <img src={starImage} alt="star" />
                     </div>
                     <span>{point}</span>
                 </div>
             </div>
-            <p className="comment">{comment}</p>
+            <p className={(styles["comment"], "paragraph-text")}>{comment}</p>
         </article>
     );
 }
