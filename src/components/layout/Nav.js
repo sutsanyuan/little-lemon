3860168d31b630d6899fc9948a71ba022cda1deb
Tsan <65704223+sutsanyuan@users.noreply.github.com>
2026-05-20 11:24:02 +0800

    footer finished, Header Nav adjustment: solved closing Nav after clicking by adding a prop(onLinkClick) , Button.js adjustment: import Link to the component and adjustment to the styles(inline-flex)

--- src/components/layout/Nav.js
+++ src/components/layout/Nav.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function Nav() {
+export default function Nav({ onLinkClick }) {
     return (
         <nav>
-            <ul>
+            {/* onLinkClick 可以個別綁在li，也可以直街綁在外層ul > Event Bubbling */}
+            <ul onClick={onLinkClick}>
                 <li>
                     <Link to="/">Home</Link>
                 </li>
