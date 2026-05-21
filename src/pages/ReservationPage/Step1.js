15ebd8949d71a59f2eb941a8d3573e6b86cf462c
Tsan <65704223+sutsanyuan@users.noreply.github.com>
2026-05-21 17:19:28 +0800

    npm install react-datepicker date-fns, designing toggleableDatePicker based on react-datepicker

--- src/pages/ReservationPage/Step1.js
+++ src/pages/ReservationPage/Step1.js
@@ -1,22 +1,12 @@
 import React from "react";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css"; // 引入基本樣式
-import "./DatePickerCustom.scss"; // 之後我們在這裡覆蓋 CSS
+import CustomDateInput from "./CustomDateInput";
+
 export default function Step1({ formData, updateFormData, nextStep }) {
     return (
         <div>
             <section className="infoSection"></section>
             <section className="formSection">
-                <div className="date-picker-wrapper">
-                    <DatePicker
-                        selected={formData.date ? new Date(formData.date) : null}
-                        onChange={(date) => updateFormData({ date: date.toISOString() })}
-                        placeholderText="Pick a date"
-                        dateFormat="yyyy/MM/dd"
-                        minDate={new Date()} // 限制不能選過去的時間
-                        inline
-                    />
-                </div>
+                <CustomDateInput formData={formData} updateFormData={updateFormData} />
             </section>
 
             <h3>Step 1: Choose Date & Time</h3>
